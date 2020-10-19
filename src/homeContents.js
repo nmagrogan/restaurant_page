91a2cd21b268132e37f981a5b036f4cf523f8eed
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2020-10-19 10:56:01 -0700

    added headline and description to the restaraunt

--- src/homeContents.js
+++ src/homeContents.js
@@ -1,17 +1,28 @@
 
 function createHomePage(){
-
+  const container = document.getElementById('container');
   addImage();
+  addHeadline();
+  addDescription();
 }
 
 function addImage(){
-  const container = document.getElementById('container');
-
-
   const img = document.createElement('img');
   img.src = 'package.png';
   img.setAttribute("style", "width:100px;height:100px;")
   container.appendChild(img);
+}
+
+function addHeadline(){
+  const headline = document.createElement('h1');
+  headline.innerHTML = "A brand new Restaurant";
+  container.appendChild(headline);
+}
+
+function addDescription(){
+  const descript = document.createElement('p');
+  descript.innerHTML = "This Restaurant is sooooo good, wow, what a place";
+  container.appendChild(descript);
 }
 
 export default createHomePage
