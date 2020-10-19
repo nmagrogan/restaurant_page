5c92e01bea0b502917b81eecd3e5a4d419fcc0b4
nmagrogan <nmagrogan@zagmail.gonzaga.edu>
2020-10-19 11:16:25 -0700

    add tabs for different pages, they don't do anything yet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import createHomePage from './homeContents.js';
 import createAboutPage from './aboutPage.js';
 import createContactPage from './contactPage.js';
 
-//createHomePage();
-//createAboutPage();
-createContactPage();
+function createTabs(){
+  const tabNames = ["Home", "About", "Contact"]
+  const container = document.getElementById('container');
+  const tabs = document.createElement('div');
+
+  for(const tab of tabNames){
+    const tabDiv = document.createElement('div');
+    tabDiv.innerHTML = tab;
+    tabDiv.classList.add("tab");
+    tabs.appendChild(tabDiv);
+  }
+  container.appendChild(tabs);
+}
+
+
+createTabs();
